4e8c46c80ecfd55884cf146c16788f3d70037e32
emilijamo <emilija.morkunaite0@gmail.com>
2025-11-26 12:19:32 +0200

    Update studentas.cpp
    Rule of three metodai pritaikomi darbui su derived klase

--- studentas.cpp
+++ studentas.cpp
@@ -5,24 +5,31 @@
 #include <sstream>
 
 Studentas::Studentas()
-    : vard_(""), pav_(""), egzas_(0), rez_vidurkis_(0.0f), rez_mediana_(0.0f) {
+    : Zmogus(), egzas_(0), rez_vidurkis_(0.0f), rez_mediana_(0.0f) {
 }
 
 Studentas::Studentas(const std::string& vardas, const std::string& pavarde)
-    : vard_(vardas), pav_(pavarde), egzas_(0), rez_vidurkis_(0.0f), rez_mediana_(0.0f) {
+    : Zmogus(vardas,pavarde), egzas_(0), rez_vidurkis_(0.0f), rez_mediana_(0.0f) {
 }
 
 Studentas::Studentas(const std::string& vardas, const std::string& pavarde, const std::vector<int>& pazymiai, int egzaminas)
-    : vard_(vardas), pav_(pavarde), paz_(pazymiai), egzas_(egzaminas) {
+    : Zmogus(vardas,pavarde), paz_(pazymiai), egzas_(egzaminas) {
     skaiciuotiRezultatus();
 }
 
 Studentas::Studentas(const Studentas& kopija)
-    : vard_(kopija.vard_), pav_(kopija.pav_), paz_(kopija.paz_), egzas_(kopija.egzas_), rez_vidurkis_(kopija.rez_vidurkis_), rez_mediana_(kopija.rez_mediana_) {
+    : Zmogus(kopija), 
+      vard_(kopija.vard_), 
+      pav_(kopija.pav_),
+      paz_(kopija.paz_), 
+      egzas_(kopija.egzas_),
+      rez_vidurkis_(kopija.rez_vidurkis_), 
+      rez_mediana_(kopija.rez_mediana_) {
 }
 
 Studentas& Studentas::operator=(const Studentas& priskiriamas) {
     if (this != &priskiriamas) {  
+        Zmogus::operator=(priskiriamas);
         vard_ = priskiriamas.vard_;
         pav_ = priskiriamas.pav_;
         paz_ = priskiriamas.paz_;
@@ -121,6 +128,7 @@ void Studentas::skaiciuotiRezultatus() {
     rez_mediana_ = egzas_ * 0.6f + mediana(paz_) * 0.4f;
 
 }
+
 
 
 
