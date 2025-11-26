776438d5d53c8a3c022b9dcf0e7979ca5e45ce79
emilijamo <emilija.morkunaite0@gmail.com>
2025-11-26 12:00:43 +0200

    Add files via upload

---
+++ zmogus.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <string>
+#include <iostream>
+
+class Zmogus {
+protected:
+    std::string vard_;
+    std::string pav_;
+
+public:
+    Zmogus() : vard_(""), pav_("") {}
+    Zmogus(const std::string& vardas, const std::string& pavarde) 
+        : vard_(vardas), pav_(pavarde) {}
+
+    virtual ~Zmogus() {
+        vard_.clear();
+        pav_.clear();
+    };
+
+    inline std::string vardas() const { return vard_; }
+    inline std::string pavarde() const { return pav_; }
+
+    virtual void skaiciuotiRezultatus() = 0;
+    virtual std::istream& readStudent(std::istream& is) = 0;
+};
